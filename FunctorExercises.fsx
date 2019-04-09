3c8df2c488ab2d5dd313fb9159902ff0e88e70e4
Channa Jayathilake <cjayathilake@liberty.com.au>
2019-04-09 10:13:17 +1000

    Some cleaning up and comments added Implement upto lengthOfContent

--- FunctorExercises.fsx
+++ FunctorExercises.fsx
@@ -9,39 +9,34 @@ let mapOption : ('a -> 'b) -> 'a option -> 'b option =
     | Some x -> Some (fn x)
     | None -> None
 
-
 // Implement map for list
-// let mapList : ('a -> 'b) -> 'a list -> 'b list =
-//   fun fn lst -> 
-//     seq {
-//       for i in lst do
-//         yield  (fn i)
-//     } 
-//     |> Seq.toList
+// This implementation uses seq computational expression. We are wrapping our list in a seqence in order to perform the map operation which is not ideal.
+let mapList' : ('a -> 'b) -> 'a list -> 'b list =
+  fun fn lst -> 
+    seq {
+      for i in lst do
+        yield  (fn i)
+    } 
+    |> Seq.toList
 
+// This implementaion relies on some state kept in each stack frame to perform each step. Which means F# compiler can't optimise this to be tail recursive.
 let rec mapList : ('a -> 'b) -> 'a list -> 'b list =
      fun fn lst -> 
-       //notImplemented ()
         match lst with
         | a::b -> fn a :: mapList fn b 
         | [] -> []
-    
-let rec mapList2 : ('a -> 'b) -> 'a list -> 'b list =
-     fun fn lst -> 
-       //notImplemented ()
-        match lst with
-        | x::b -> (fn x) :: mapList2 fn b 
-        | [] -> []
-        //| [] -> [fn a]
-    
 
+// TODO : implement tail recursive map for list  
+let rec mapList'' : ('a -> 'b) -> 'a list -> 'b list =
+     fun fn lst -> 
+       notImplemented ()
+       
 // Implement map for Result
 let mapResult : ('a -> 'b) -> Result<'a, 'c> -> Result<'b, 'c> =
   fun fn res ->
     match res with
     | Ok a -> fn a |> Ok
     | Error e -> Error e
-
 
 // Implement map for Result's error
 let mapResultError : ('a -> 'b) -> Result<'c, 'a> -> Result<'c, 'b> =
@@ -50,7 +45,6 @@ let mapResultError : ('a -> 'b) -> Result<'c, 'a> -> Result<'c, 'b> =
     | Ok a -> Ok a
     | Error e -> fn e |> Error
 
-
 // Implement bimap for Result. You should be able to implement it using functions you've defined previously
 let bimapResult : ('a -> 'b) -> ('c -> 'd) -> Result<'a, 'c> -> Result<'b, 'd> =
   fun first second res ->
@@ -58,20 +52,18 @@ let bimapResult : ('a -> 'b) -> ('c -> 'd) -> Result<'a, 'c> -> Result<'b, 'd> =
     | Ok a -> first a |> Ok
     | Error e -> second e |> Error
 
-
-// Implement bimap for Result. You should be able to implement it using functions you've defined previously
-let bimapResult2 : ('a -> 'b) -> ('c -> 'd) -> Result<'a, 'c> -> Result<'b, 'd> =
+let bimapResult' : ('a -> 'b) -> ('c -> 'd) -> Result<'a, 'c> -> Result<'b, 'd> =
   fun first second res ->
     match res with
     | Ok a -> mapResult first (Ok a)
     | Error e -> mapResultError second (Error e)
 
-// Implement bimap for Result. You should be able to implement it using functions you've defined previously
-let bimapResult3 : ('a -> 'b) -> ('c -> 'd) -> Result<'a, 'c> -> Result<'b, 'd> =
+let bimapResult'' : ('a -> 'b) -> ('c -> 'd) -> Result<'a, 'c> -> Result<'b, 'd> =
   fun first second res ->
     mapResult first res |> mapResultError second
 
- // Implement map for choice 3
+
+// Implement map for choice 3
 let mapChoice3 : ('a -> 'b) -> Choice<'a, 'c, 'd> -> Choice<'b, 'c, 'd> =
   fun fn lst ->
     match lst with
@@ -82,16 +74,16 @@ let mapChoice3 : ('a -> 'b) -> Choice<'a, 'c, 'd> -> Choice<'b, 'c, 'd> =
 // Implement map for Function
 type Function<'Input, 'Output> = Function of ('Input -> 'Output)
 
-let _mapFunction : ('a -> 'b) -> ('x -> 'a) -> ('x -> 'b) =
+let mapFunction : ('a -> 'b) -> ('x -> 'a) -> ('x -> 'b) =
   fun fn f ->
       f >> fn 
 
-let mapFunction : ('a -> 'b) -> Function<'x, 'a> -> Function<'x, 'b> =
+let mapFunction' : ('a -> 'b) -> Function<'x, 'a> -> Function<'x, 'b> =
   fun fn f ->
       match f with | Function ff -> Function (ff >> fn)
 
 
-let __mapFunction : ('a -> 'b) -> Function<'x, 'a> -> Function<'x, 'b> =
+let mapFunction'' : ('a -> 'b) -> Function<'x, 'a> -> Function<'x, 'b> =
   fun fn (Function f) ->
        Function (f >> fn)
 
@@ -103,7 +95,7 @@ let mapAsync : ('a -> 'b) -> 'a Async -> 'b Async =
      return fn va
   }
 
-// Without using the compiler, what is the type of somethingElse?
+// Without using the compiler, what is the type of somethingElse? int option
 let stringLength : string -> int = String.length
 let something = None
 let somethingElse = mapOption stringLength something
@@ -132,6 +124,7 @@ let id : 'a -> 'a = fun x -> x
 let endingResult = mapResult id startingResult
 
 
+// TODO :
 // Is this function Functor's map function? Explain why yes or no.
 let mapOptionInts : (int -> int) -> int option -> int option =
   fun fn opt -> notImplemented () // You don't need to see the implementation
@@ -151,9 +144,9 @@ let stringLength' : string -> int = String.length
 
 let lengthOfContent : Uri -> MyAsync<int> =
   fun uri ->
-    notImplemented ()
+     uri |> httpGet |> mapMyAsync (fun r -> stringLength' r.Content)
 
-
+ 
 // How could you refactor refactorMe to use maps?
 let readFile : string -> Async<byte[]> = notImplemented ()
 let writeFile : string -> string -> Async<unit> = notImplemented ()
