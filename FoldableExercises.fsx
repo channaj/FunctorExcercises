af1cb72f9aab490d0d500f3d10db641454d903e8
Channa Jayathilake <cjayathilake@liberty.com.au>
2019-05-30 09:40:22 +1000

    upto betterPartition

--- FoldableExercises.fsx
+++ FoldableExercises.fsx
@@ -41,12 +41,12 @@ let repeatTailRecursive : int -> 'a -> MyList<'a> =
 // eg. countDownFrom 3 = Cons (3,Cons (2,Cons (1,Cons (0,EndOfList))))
 let countDownFrom : int -> MyList<int> =
   fun start ->
-    let rec step lst i =
-     if i <= start then
-      step (Cons (i, lst)) (i+1)
-     else
-      lst
-    step EndOfList 0
+    let rec step i =
+      if i >= 0 then
+        Cons (i,step (i-1))
+      else
+        EndOfList 
+    step start 
 
 
 // Is your `countDownFrom` tail recursive? If not, make it so below.
@@ -66,7 +66,11 @@ let countDownFromTailRecursive : int -> MyList<int> =
 // and returns the total. Use recursion.
 let sum : MyList<int> -> int =
   fun ints ->
-    notImplemented ()
+    let rec step lst =
+      match lst with
+      | EndOfList -> 0
+      | Cons (x, xs) -> x + step xs
+    step ints
 
 
 // Make your `sum` tail recursive if it is not already.
@@ -74,15 +78,22 @@ let sum : MyList<int> -> int =
 // your previous one to be _not_ tail recusive for comparison.
 let sumTailRecursive : MyList<int> -> int =
   fun ints ->
-    notImplemented ()
+    let rec step lst i =
+      match lst with
+      | EndOfList -> i
+      | Cons (x, xs) -> step xs (i+x)
+    step ints 0
 
 
 // Implement a function that takes a list and returns it in reverse
 // order. Make sure your implementation is tail recursive.
 let reverseList : MyList<'a> -> MyList<'a> =
   fun lst ->
-    notImplemented ()
-
+    let rec step lst' out =
+      match lst' with
+      | EndOfList -> out
+      | Cons (x, xs) -> step xs (Cons (x, out))
+    step lst EndOfList
 
 // Implement the partition function using tail recursion. Partition uses a
 // predicate function to split a list into two sublists. The order of the
@@ -93,8 +104,20 @@ let reverseList : MyList<'a> -> MyList<'a> =
 // HINT: You will need to use your reverseList function
 let partition : ('a -> bool) -> MyList<'a> -> MyList<'a> * MyList<'a> =
   fun predicate lst ->
-    notImplemented ()
+    let rec step lst' out =
+      match lst', out with
+      | EndOfList, _ -> out
+      | Cons (x, xs), (o1, o2) -> 
+          if (predicate x) then
+            step xs (Cons (x, o1), o2)
+          else
+            step xs (o1, Cons (x, o2))
+    let (a,b) = step lst (EndOfList, EndOfList)
+    (reverseList a, reverseList b)
 
+// let isOdd x = x % 2 = 1
+// let lst = countDownFromTailRecursive 10
+// let test = partition isOdd lst
 
 // You might have noticed a common way all these different tail-recursive functions work.
 // They all walk the list recursively and accumulate some state they pass forward
@@ -103,26 +126,43 @@ let partition : ('a -> bool) -> MyList<'a> -> MyList<'a> * MyList<'a> =
 // Implement the fold function for MyList, tail recursively.
 let fold : ('state -> 'a -> 'state) -> 'state -> MyList<'a> -> 'state =
   fun folder initialState lst ->
-    notImplemented ()
+    let rec step state lst' =
+      match lst' with
+      | EndOfList -> state
+      | Cons (x, xs) -> step (folder state x) xs
+    step initialState lst
 
 
 // Reimplement sum using fold
 let sumUsingFold : MyList<int> -> int =
   fun lst ->
-    notImplemented ()
+    fold (+) 0 lst
 
+// let lst = countDownFromTailRecursive 5
+// let total = sumUsingFold lst
 
 // Reimplement reverseList using fold
 let reverseListUsingFold : MyList<'a> -> MyList<'a> =
   fun lst ->
-    notImplemented ()
+   fold (fun x y -> Cons (y, x)) EndOfList lst
 
 
 // Reimplement partition using fold
 let partitionUsingFold : ('a -> bool) -> MyList<'a> -> MyList<'a> * MyList<'a> =
   fun predicate lst ->
-    notImplemented ()
+    let (a,b) = 
+      fold 
+        (fun (o1, o2) x ->
+          if (predicate x) then 
+            Cons (x, o1), o2
+          else 
+            o1, Cons (x, o2)
+        ) (EndOfList, EndOfList) lst
+    (reverseList a, reverseList b)
 
+// let isOdd x = x % 2 = 1
+// let lst = countDownFromTailRecursive 10
+// let test = partitionUsingFold isOdd lst
 
 // A different version of partition that would indicate its behaviour better
 // in the types and also be more generic would use a Choice type instead of
@@ -130,7 +170,23 @@ let partitionUsingFold : ('a -> bool) -> MyList<'a> -> MyList<'a> * MyList<'a> =
 // Implement this function using a fold.
 let betterPartition : ('a -> Choice<'b,'c,'d>) -> MyList<'a> -> MyList<'b> * MyList<'c> * MyList<'d> =
   fun makeChoice lst ->
-    notImplemented ()
+    let (a, b, c) = 
+      fold 
+        (fun (o1, o2, o3) x ->
+          match makeChoice x with
+          | Choice1Of3 a -> (Cons (a,o1), o2, o3)
+          | Choice2Of3 b -> (o1, Cons (b,o2), o3)
+          | Choice3Of3 c -> (o1, o2, Cons(c,o3))
+        ) (EndOfList, EndOfList, EndOfList) lst
+    (reverseList a, reverseList b, reverseList c)
+
+let chooser x = 
+  if x > 7 then Choice1Of3 <| sprintf "%A > 7" x
+  elif x % 2 = 0 then Choice2Of3 (x,"Even")
+  else Choice3Of3 x
+
+let lst = countDownFromTailRecursive 10
+let test = betterPartition chooser lst
 
 // Folds are not limited to linked lists. You can fold over many different types.
 // Write a fold for an array using tail recursion.
